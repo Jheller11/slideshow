aa4db88ac577b34cb13db4ebe829290098a69dd3
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-09-21 16:26:03 -0400

    code and debug slideshow/slide classes

--- index.js
+++ index.js
@@ -78,3 +78,61 @@ const shootingStar = () => {
 // start on page load
 
 createStars(100)
+
+// Slideshow
+
+let sections = ['welcome', 'a', 'b', 'c', 'd']
+
+class Slide {
+  constructor(element) {
+    this.element = element.id
+    this.top = element.offsetTop
+  }
+
+  smoothScroll() {
+    window.scroll({
+      top: this.top,
+      behavior: 'smooth'
+    })
+  }
+}
+
+class Slideshow {
+  constructor(array, event, slides = [], activeSlide = 0, nextSlide = 1) {
+    this.array = array
+    this.event = event
+    this.slides = slides
+    this.activeSlide = activeSlide
+    this.nextSlide = nextSlide
+  }
+
+  createSlideshow() {
+    let slides = []
+    for (let i = 0; i < this.array.length; i++) {
+      let element = document.querySelector('#' + this.array[i])
+      let newSlide = new Slide(element)
+      slides.push(newSlide)
+    }
+    this.slides = slides
+    this.addEvent()
+  }
+
+  scrollToNextSlide() {
+    if (this.nextSlide === this.slides.length) {
+      this.slides[0].smoothScroll()
+      this.activeSlide = 0
+      this.nextSlide = 1
+    } else {
+      this.slides[this.nextSlide].smoothScroll()
+      this.activeSlide += 1
+      this.nextSlide += 1
+    }
+  }
+
+  addEvent() {
+    window.addEventListener(this.event, this.scrollToNextSlide.bind(this))
+  }
+}
+
+const mySlideShow = new Slideshow(sections, 'click')
+mySlideShow.createSlideshow()
