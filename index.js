90d411abcb7355e4d77bb73c225d1abeba90a4be
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-09-21 18:16:57 -0400

    move stars js, add getactiveslide method on user scrolling

--- index.js
+++ index.js
@@ -1,85 +1,4 @@
-// background div
-
-const starContainer = document.querySelector('.star-container')
-
-// define star class
-class Star {
-  constructor(position, size, color, blinking) {
-    this.position = position
-    this.size = size
-    this.color = color
-    this.blinking = blinking
-  }
-
-  //   generate string for inline style when element added
-  style() {
-    let string = `top: ${this.position[0]}px; left: ${
-      this.position[1]
-    }px; background-color: ${this.color}; width: ${this.size}px; height: ${
-      this.size
-    }px; ${
-      this.blinking ? 'animation: blink 2s infinite ease-in-out' : ''
-    }; animation-delay: ${random(0, 4)}s;`
-    return string
-  }
-}
-
-// random num generator for star properties
-const random = (min, max) => {
-  return Math.floor(Math.random() * max) + min
-}
-
-// animate?
-const animate = () => {
-  let blinking = Math.random() >= 0.5
-  return blinking
-}
-
-// color?
-const color = () => {
-  let color = Math.random()
-  if (color >= 0.33) {
-    return '0, 0%, 100%'
-  } else {
-    return color >= 0.165 ? '0, 59%, 70%' : '230, 59%, 70%'
-  }
-}
-
-// star generator
-const createStars = amount => {
-  for (let i = amount; i > 0; i--) {
-    let star = new Star(
-      [random(10, 690), random(10, screen.width - 150)],
-      random(2, 2),
-      `hsl( ${color()})`,
-      animate()
-    )
-    let div = document.createElement('div')
-    div.setAttribute('class', 'star')
-    div.setAttribute('style', star.style())
-    starContainer.appendChild(div)
-  }
-}
-
-// shooting star generator
-
-const shootingStar = () => {
-  let shooter = new Star(
-    [random(100, -200), random(100, -200)],
-    random(2, 2),
-    `hsl( ${color()})`
-  )
-  let div = document.createElement('div')
-  div.setAttribute('class', 'shooting-star')
-  div.setAttribute('style', shooter.style())
-  starContainer.appendChild(div)
-}
-
-// start on page load
-
-createStars(100)
-
-// Slideshow
+// Slideshow -------------------------------------------------------------------------
 
 let sections = ['welcome', 'a', 'b', 'c', 'd']
 
@@ -114,7 +33,7 @@ class Slideshow {
       slides.push(newSlide)
     }
     this.slides = slides
-    this.addEvent()
+    this.addEvents()
   }
 
   scrollToNextSlide() {
@@ -129,10 +48,30 @@ class Slideshow {
     }
   }
 
-  addEvent() {
+  addEvents() {
     window.addEventListener(this.event, this.scrollToNextSlide.bind(this))
+    window.addEventListener('scroll', () => {
+      this.activeSlide = this.getActiveSlide()
+      this.nextSlide = this.activeSlide + 1
+    })
+  }
+
+  getActiveSlide() {
+    let tops = this.slides.map(slide => {
+      return slide.top
+    })
+    let scrollPosition = window.scrollY
+    let arr = []
+    tops.forEach((top, i) => {
+      if (scrollPosition >= top - 1) {
+        arr.push(i)
+      }
+    })
+    return arr[arr.length - 1]
   }
 }
 
 const mySlideShow = new Slideshow(sections, 'click')
 mySlideShow.createSlideshow()
+
+// -------------------------------------------------------------------------------
---
+++ stars.js
@@ -0,0 +1,83 @@
+// background div
+
+const starContainer = document.querySelector('.star-container')
+
+// define star class
+class Star {
+  constructor(position, size, color, blinking) {
+    this.position = position
+    this.size = size
+    this.color = color
+    this.blinking = blinking
+  }
+
+  //   generate string for inline style when element added
+  style() {
+    let string = `top: ${this.position[0]}px; left: ${
+      this.position[1]
+    }px; background-color: ${this.color}; width: ${this.size}px; height: ${
+      this.size
+    }px; ${
+      this.blinking ? 'animation: blink 2s infinite ease-in-out' : ''
+    }; animation-delay: ${random(0, 4)}s;`
+    return string
+  }
+}
+
+// random num generator for star properties
+const random = (min, max) => {
+  return Math.floor(Math.random() * max) + min
+}
+
+// animate?
+const animate = () => {
+  let blinking = Math.random() >= 0.5
+  return blinking
+}
+
+// color?
+const color = () => {
+  let color = Math.random()
+  if (color >= 0.33) {
+    return '0, 0%, 100%'
+  } else {
+    return color >= 0.165 ? '0, 59%, 70%' : '230, 59%, 70%'
+  }
+}
+
+// star generator
+const createStars = amount => {
+  for (let i = amount; i > 0; i--) {
+    let star = new Star(
+      [random(10, 690), random(10, screen.width - 150)],
+      random(2, 2),
+      `hsl( ${color()})`,
+      animate()
+    )
+    let div = document.createElement('div')
+    div.setAttribute('class', 'star')
+    div.setAttribute('style', star.style())
+    starContainer.appendChild(div)
+  }
+}
+
+// shooting star generator
+
+const shootingStar = () => {
+  let shooter = new Star(
+    [random(100, -200), random(100, -200)],
+    random(2, 2),
+    `hsl( ${color()})`
+  )
+  let div = document.createElement('div')
+  div.setAttribute('class', 'shooting-star')
+  div.setAttribute('style', shooter.style())
+  starContainer.appendChild(div)
+}
+
+// start on page load
+
+createStars(100)
+shootingStar()
+shootingStar()
+shootingStar()
