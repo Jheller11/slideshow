777889ffbd41bcc42b58208107abb04fdd240a2e
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-10-05 17:35:05 -0400

    clean up code

--- lightning/lightning.js
+++ lightning/lightning.js
@@ -1,10 +1,8 @@
 const container = document.querySelector('.container')
 
 const bolt = index => {
-  let div = document.createElement('div')
-  div.setAttribute('class', 'bolt')
-  div.innerHTML = '<svg width="900" height="700"></svg>'
-  container.appendChild(div)
+  let div = document.querySelector('.bolt')
+  div.innerHTML += '<svg width="900" height="700"></svg>'
   let svg = document.querySelectorAll('svg')
   svg[index].innerHTML = generatePath()
 }
@@ -24,3 +22,6 @@ const generatePath = () => {
   let string = `<path stroke="yellow" fill="transparent" d="${pathString}" />`
   return string
 }
+
+bolt(0)
+bolt(1)
