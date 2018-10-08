7c014110c0835e24dce3bda9e4629dc35df6e651
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-10-08 14:48:45 -0400

    switch to clouds

--- lightning/lightning.js
+++ lightning/lightning.js
@@ -1,35 +1,38 @@
-const settings = {
-  frequency: 2000,
-  number: 2
-}
+// const settings = {
+//   boltFrequency: 5000,
+//   boltNumber: 1,
+//   flashFrequency: 2000
+// }
 
-const bolt = index => {
-  let div = document.querySelector('.bolt')
-  div.innerHTML += '<svg width="900" height="700"></svg>'
-  let svg = document.querySelectorAll('svg')
-  svg[index].innerHTML = generatePath()
-}
+// const bolt = index => {
+//   let div = document.querySelector('.bolt')
+//   div.innerHTML += '<svg width="900" height="700"></svg>'
+//   let svg = document.querySelectorAll('svg')
+//   svg[index].innerHTML = generatePath()
+// }
 
-const random = (min, range) => {
-  return Math.floor(Math.random() * range) + min
-}
+// const random = (min, range) => {
+//   return Math.floor(Math.random() * range) + min
+// }
 
-const generatePath = () => {
-  let pathString = ''
-  let points = []
-  points.push('M' + random(15, 700) + ' ' + random(10, 100))
-  for (let i = 0; i < 20; i++) {
-    points.push('l' + random(-40, 80) + ' ' + random(20, 20))
-  }
-  pathString += points.join('')
-  let string = `<path stroke="yellow" fill="transparent" d="${pathString}" />`
-  return string
-}
+// const generatePath = () => {
+//   let pathString = ''
+//   let points = []
+//   // random starting point
+//   points.push('M' + random(15, 700) + ' ' + random(10, 100))
+//   //   random points
+//   for (let i = 0; i < 15; i++) {
+//     points.push('l' + random(-40, 80) + ' ' + random(40, 20))
+//   }
+//   pathString += points.join('')
+//   let string = `<path stroke="yellow" fill="transparent" d="${pathString}" />`
+//   return string
+// }
 
-const initiate = () => {
-  for (let i = 0; i < settings.number; i++) {
-    setInterval(() => bolt(i), settings.frequency)
-  }
-}
+// const initiate = () => {
+//   for (let i = 0; i < settings.boltNumber; i++) {
+//     setInterval(() => bolt(i), settings.boltFrequency)
+//   }
+// }
 
-initiate()
+// initiate()
