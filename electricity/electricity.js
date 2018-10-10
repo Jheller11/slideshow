c028ae1dab4b28b58284fd54f9c2506c63e96aa9
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-10-10 15:46:49 -0400

    add random lines between circles

--- electricity/electricity.js
+++ electricity/electricity.js
@@ -1,18 +1,17 @@
-let circle = document.querySelector('circle')
-let svg = document.querySelector('svg')
+const svg = document.querySelector('svg')
 
 // calculate a set of points along the circles
 const calcPoint = (angle, radius) => {
   let x = radius * Math.sin(angle)
   let y = radius * Math.cos(angle)
-  return [x, y]
+  return x.toString() + ' ' + y.toString()
 }
 const outerCirclePoints = []
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 200; i++) {
   outerCirclePoints.push(calcPoint(i, 200))
 }
 const innerCirclePoints = []
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 200; i++) {
   innerCirclePoints.push(calcPoint(i, 20))
 }
 
@@ -21,3 +20,28 @@ for (let i = 0; i < 100; i++) {
 const random = max => {
   return Math.floor(Math.random() * max)
 }
+
+// generate a line
+
+const line = () => {
+  let path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  path.setAttribute('stroke-width', '1')
+  path.setAttribute('stroke', 'orange')
+  path.setAttribute('fill', 'transparent')
+  path.setAttribute(
+    'd',
+    `M250 250l${innerCirclePoints[random(200)]}l${
+      outerCirclePoints[random(200)]
+    }`
+  )
+  console.log(path)
+  svg.appendChild(path)
+}
+
+line()
+line()
+line()
+
+// add new lines to page
+setInterval(() => line(), 2500)
+// remove lines from page
